2b2ff5da1f00cb1a7575ff971b2b72b8332f2c28
M1kxy <samik08dey@gmail.com>
2026-03-03 17:49:53 +0530

    Initialize Spring Boot project with base configuration

---
+++ src/main/java/com/lending/platform/loan_evaluation/LoanEvaluationApplication.java
@@ -0,0 +1,13 @@
+package com.lending.platform.loan_evaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanEvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanEvaluationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lending/platform/loan_evaluation/LoanEvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lending.platform.loan_evaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanEvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
